10f1e5f37fb8519d7a49a8286d27137ff41d7ad1
DirtKingy <92637772+DirtKingy@users.noreply.github.com>
2025-06-25 12:00:20 +0200

    updated the DonutApi model to include Hasfactory for tests. a $table to make sure it has the correct table and a $fillable to make sure things can be created or deleted

--- app/Models/DonutApi.php
+++ app/Models/DonutApi.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class DonutApi extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'donuts';
+
+    protected $fillable = [
+        'name',
+        'seal_of_approval',
+        'price'
+    ];
 }
